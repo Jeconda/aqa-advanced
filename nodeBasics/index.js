f426ecad58b54c7d452f57bafacd162df0280e39
Evgenii <tsibulko2018@gmail.com>
2025-07-24 18:12:49 +0300

    chore: Update nodeBasics index.js

--- nodeBasics/index.js
+++ nodeBasics/index.js
@@ -1,7 +1,5 @@
-// Імпортуємо бібліотеку chalk
 import chalk from 'chalk';
 
-// Виводимо кольоровий текст у консоль
 console.log(chalk.blue('Привіт, світ Git та Node.js!'));
 console.log(chalk.red.bold('Це мій перший кольоровий текст з Chalk.'));
 console.log(chalk.green('Успішне виконання домашнього завдання!'));
