36880e97ccc8d1836142462e5b4f5ebbc50e759f
Evgenii <tsibulko2018@gmail.com>
2025-07-29 17:57:10 +0300

    feat: Complete JS Conditions and Loops homework, add module type

---
+++ jsConditionsLoops/task3.js
@@ -0,0 +1,10 @@
+const number = 8;
+for (let i = 1; i <= 10; i++) {
+    console.log(`${number} x ${i} = ${number * i}`)
+}
+console.log('Multiplication table cycle "while"');
+let j = 1 
+    while (j <= 10) {
+      console.log(`${number} x ${j} = ${number * j}`);
+      j++   
+}
